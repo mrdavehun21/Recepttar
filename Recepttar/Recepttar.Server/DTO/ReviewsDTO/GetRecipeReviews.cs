e4710aa31b0bd0b12957a45aedbec1a28ac9c3ba
mrdavehun21 <ernhofferd@gmail.com>
2026-01-22 17:25:09 +0100

    Add correct getrecipereview

--- Recepttar/Recepttar.Server/DTO/ReviewsDTO/GetRecipeReviews.cs
+++ Recepttar/Recepttar.Server/DTO/ReviewsDTO/GetRecipeReviews.cs
@@ -2,7 +2,6 @@
 {
     public class GetRecipeReviews
     {
-        public int Id { get; set; }
         public int RecipeId { get; set; }
         public int UserId { get; set; }
         public int Stars { get; set; }
