733943c4e89d1fcafc4264f7b6ab83ddca92df0b
Ernhoffer Dávid <ernhofferd@gmail.com>
2026-02-22 19:29:17 +0100

    Add CreatedAt property to CreateRecipeDto

--- Recepttar/Recepttar.Server/DTOs/Recipe/CreateRecipeDto.cs
+++ Recepttar/Recepttar.Server/DTOs/Recipe/CreateRecipeDto.cs
@@ -14,6 +14,7 @@ namespace Recepttar.Server.DTOs.Recipe
         public bool IsExpensive { get; set; }
         public bool IsVegan { get; set; }
         public RecipeTypeEnum Type { get; set; }
+        public DateTime CreatedAt { get; set; }
         public IFormFile? DishPicture { get; set; }
 
         public List<IngredientDto> Ingredients { get; set; }
