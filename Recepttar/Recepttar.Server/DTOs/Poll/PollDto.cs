f0de865ccae7ea0b477fada920d8d030c05f8342
Ernhoffer Dávid <ernhofferd@gmail.com>
2026-02-22 18:57:42 +0100

    Add AuthorId property to PollDto

--- Recepttar/Recepttar.Server/DTOs/Poll/PollDto.cs
+++ Recepttar/Recepttar.Server/DTOs/Poll/PollDto.cs
@@ -3,6 +3,7 @@
     public class PollDto
     {
         public int Id { get; set; }
+        public int AuthorId { get; set; }
         public string Question { get; set; } = string.Empty;
         public List<PollOptionDto> Options { get; set; }
         public int? VotedOn { get; set; }
