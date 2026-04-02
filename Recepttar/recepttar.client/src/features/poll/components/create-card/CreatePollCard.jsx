0452b1c76754ad4b9bfc5c742eec4e958a6b3186
VokanicsKristof <vokanics.kristof@blathy.info>
2026-04-02 12:30:10 +0200

    Bug fix for favorites on main page, adding create poll card to my-collection page

--- Recepttar/recepttar.client/src/features/poll/components/create-card/CreatePollCard.jsx
+++ Recepttar/recepttar.client/src/features/poll/components/create-card/CreatePollCard.jsx
@@ -1,9 +1,9 @@
 import '../../../main/components/poll-section/PollCard.css';
 
-function CreatePollCard({ openFormTrigger }) {
+function CreatePollCard({ openFormTrigger, caption="Create your own!" }) {
   return (
     <div className="card overflow-hidden shadow m-3 rounded-3 vote-card d-flex justify-content-evenly h-fill-available">
-      <h3 className="ms-auto me-auto mt-3 mb-4">Create your own!</h3>
+      <h3 className="ms-auto me-auto mt-3 mb-4">{caption}</h3>
       <div className="bg-light border shadow ms-auto me-auto mb-4 d-flex justify-content-center align-items-center rounded-circle" style={{width: "80px", height: "80px" }} onClick={openFormTrigger}>
         <i className="bi bi-plus fs-1"></i>
       </div>
